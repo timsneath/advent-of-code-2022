417bad4eb7b8c529f1ce9c352778e76fb0fc863c
Tim Sneath <tim@sneath.org>
2022-11-30 20:38:09 -0800

    Fake

--- day01/day01.dart
+++ day01/day01.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
 
+int doStuff(List<String> rawData) {
+  return rawData.length;
+}
+
 // coverage:ignore-start
 void main(List<String> args) {
   final path = args.isNotEmpty ? args[0] : 'day01/day01.txt';
   final rawData = File(path).readAsLinesSync();
 
-  // Do stuff
+  doStuff(rawData);
 }
 // coverage:ignore-end
--- test/day01_test.dart
+++ test/day01_test.dart
@@ -1,7 +1,13 @@
 import 'package:test/test.dart';
 
+import '../day01/day01.dart';
+
 void main() {
-  group('Part 1', () {});
+  group('Part 1', () {
+    test('test name', () {
+      expect(doStuff([]), isZero);
+    });
+  });
 
   group('Part 2', () {});
 }
