b7fee100a5149f0acdc3035f64d58fc4532da851
Tim Sneath <tim@sneath.org>
2022-11-30 21:20:51 -0800

    Day 1 challenge

--- day01/day01.dart
+++ day01/day01.dart
@@ -1,14 +1,33 @@
 import 'dart:io';
 
-int doStuff(List<String> rawData) {
-  return rawData.length;
+import 'package:collection/collection.dart';
+
+List<int> countElfCalories(List<String> rawData) {
+  final elfCalories = <int>[];
+  var calories = 0;
+
+  for (final calorieItem in rawData) {
+    if (calorieItem.isNotEmpty) {
+      calories += int.parse(calorieItem);
+    } else {
+      elfCalories.add(calories);
+      calories = 0;
+    }
+  }
+  return elfCalories;
 }
 
-// coverage:ignore-start
+int mostCalorificElf(List<String> rawData) => countElfCalories(rawData).max;
+
+int topThreeCalorificElves(List<String> rawData) {
+  final elvesSortedByCalories = countElfCalories(rawData)..sort();
+  return elvesSortedByCalories.reversed.take(3).sum;
+}
+
 void main(List<String> args) {
   final path = args.isNotEmpty ? args[0] : 'day01/day01.txt';
   final rawData = File(path).readAsLinesSync();
 
-  doStuff(rawData);
+  print(mostCalorificElf(rawData));
+  print(topThreeCalorificElves(rawData));
 }
-// coverage:ignore-end
--- test/day01_test.dart
+++ test/day01_test.dart
@@ -3,15 +3,27 @@ import 'package:test/test.dart';
 import '../day01/day01.dart';
 
 void main() {
-  group('Part 1', () {
-    test('test 1', () {
-      expect(doStuff([]), isZero);
-    });
+  test('Elf calorie count', () {
+    expect(
+        mostCalorificElf([
+          '1000', '2000', '3000', '', // elf 1
+          '4000', '', // elf 2
+          '5000', '6000', '', // elf 3
+          '7000', '8000', '9000', '', // elf 4
+          '10000', '' // elf 5
+        ]),
+        equals(24000));
   });
 
-  group('Part 2', () {
-    test('test 2', () {
-      expect(doStuff(['a', 'b', 'c']), equals(3));
-    });
+  test('Top 3 elves calorie count', () {
+    expect(
+        topThreeCalorificElves([
+          '1000', '2000', '3000', '', // elf 1
+          '4000', '', // elf 2
+          '5000', '6000', '', // elf 3
+          '7000', '8000', '9000', '', // elf 4
+          '10000', '' // elf 5
+        ]),
+        equals(45000));
   });
 }
