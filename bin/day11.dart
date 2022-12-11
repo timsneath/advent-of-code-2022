c7493c9ea3b7c8ae37ddd04919defbd637e00f95
Tim Sneath <tim@sneath.org>
2022-12-10 22:13:43 -0800

    Day 11 Part 1

---
+++ bin/day11.dart
@@ -0,0 +1,134 @@
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+
+enum Operator { plus, multiply, square }
+
+class Operation {
+  final Operator operator;
+  final int operand;
+
+  const Operation(this.operator, this.operand);
+
+  factory Operation.fromString(String function) {
+    final words = function.split(' ').reversed.toList();
+    if (words.first == 'old' && words[1] == '*') {
+      return const Operation(Operator.square, 0);
+    } else {
+      return Operation(words[1] == '*' ? Operator.multiply : Operator.plus,
+          int.parse(words.first));
+    }
+  }
+  int calculate(int old) {
+    if (operator == Operator.square) return old * old;
+    if (operator == Operator.multiply) return old * operand;
+    return old + operand;
+  }
+}
+
+class ThrownItem {
+  final int thrownToMonkey;
+  final int worryLevel;
+
+  const ThrownItem(this.thrownToMonkey, this.worryLevel);
+}
+
+class Monkey {
+  final int id;
+  final Queue<int> worryLevels;
+  final Operation operation;
+  final int testModulo;
+  final int throwIfTrue;
+  final int throwIfFalse;
+  bool divideByThreeRule;
+  int itemsInspected = 0;
+
+  Monkey(this.id, this.worryLevels, this.operation, this.testModulo,
+      this.throwIfTrue, this.throwIfFalse, this.divideByThreeRule);
+
+  factory Monkey.fromString(List<String> monkey,
+      {bool divideByThreeRule = true}) {
+    final id = int.parse(monkey[0].substring(7, 8));
+    final items =
+        Queue<int>.from(monkey[1].split(': ').last.split(', ').map(int.parse));
+    final operation = Operation.fromString(monkey[2]);
+    final testModulo = int.parse(monkey[3].split(' ').last);
+    final throwIfTrue = int.parse(monkey[4].split(' ').last);
+    final throwIfFalse = int.parse(monkey[5].split(' ').last);
+    return Monkey(id, items, operation, testModulo, throwIfTrue, throwIfFalse,
+        divideByThreeRule);
+  }
+
+  ThrownItem processItem(int oldWorryLevel) {
+    itemsInspected++;
+    final newWorryLevel = operation.calculate(oldWorryLevel);
+    final boredWorryLevel =
+        divideByThreeRule ? newWorryLevel ~/ 3 : newWorryLevel;
+    final newMonkey =
+        boredWorryLevel % testModulo == 0 ? throwIfTrue : throwIfFalse;
+    return ThrownItem(newMonkey, boredWorryLevel);
+  }
+
+  Iterable<ThrownItem> processItems() {
+    final processedItems = <ThrownItem>[];
+
+    while (worryLevels.isNotEmpty) {
+      final worryLevel = worryLevels.removeFirst();
+      processedItems.add(processItem(worryLevel));
+    }
+
+    return processedItems;
+  }
+}
+
+class Monkeys {
+  late final List<Monkey> monkeys;
+
+  int get length => monkeys.length;
+  Monkey get first => monkeys.first;
+  Monkey get last => monkeys.last;
+
+  Monkeys.fromData(List<String> data, {bool divideByThreeRule = true}) {
+    monkeys = data
+        .slices(7)
+        .map((m) => Monkey.fromString(m, divideByThreeRule: divideByThreeRule))
+        .toList();
+  }
+
+  void processRound() {
+    for (final monkey in monkeys) {
+      final itemsThrown = monkey.processItems();
+      for (final item in itemsThrown) {
+        monkeys[item.thrownToMonkey].worryLevels.add(item.worryLevel);
+      }
+    }
+  }
+
+  void processRounds(int rounds) {
+    for (var i = 0; i < rounds; i++) {
+      processRound();
+    }
+  }
+
+  List<int> get itemsInspected => monkeys.map((m) => m.itemsInspected).toList();
+
+  int topTwoMonkeysProduct() {
+    final inspected = (itemsInspected..sort()).reversed.toList();
+    return inspected[0] * inspected[1];
+  }
+
+  @override
+  String toString() => monkeys
+      .map((m) => 'Monkey ${m.id}: ${m.worryLevels.toList().toString()}')
+      .join('\n');
+}
+
+// coverage:ignore-start
+void main(List<String> args) {
+  final path = args.isNotEmpty ? args[0] : 'data/day11.txt';
+  final data = File(path).readAsLinesSync();
+  final monkeys = Monkeys.fromData(data)..processRounds(20);
+  print(monkeys.topTwoMonkeysProduct());
+}
+// coverage:ignore-end
---
+++ test/day11_test.dart
@@ -0,0 +1,122 @@
+import 'package:test/test.dart';
+
+import '../bin/day11.dart';
+
+const testData = <String>[
+  'Monkey 0:',
+  '  Starting items: 79, 98',
+  '  Operation: new = old * 19',
+  '  Test: divisible by 23',
+  '    If true: throw to monkey 2',
+  '    If false: throw to monkey 3',
+  '',
+  'Monkey 1:',
+  '  Starting items: 54, 65, 75, 74',
+  '  Operation: new = old + 6',
+  '  Test: divisible by 19',
+  '    If true: throw to monkey 2',
+  '    If false: throw to monkey 0',
+  '',
+  'Monkey 2:',
+  '  Starting items: 79, 60, 97',
+  '  Operation: new = old * old',
+  '  Test: divisible by 13',
+  '    If true: throw to monkey 1',
+  '    If false: throw to monkey 3',
+  '',
+  'Monkey 3:',
+  '  Starting items: 74',
+  '  Operation: new = old + 3',
+  '  Test: divisible by 17',
+  '    If true: throw to monkey 0',
+  '    If false: throw to monkey 1',
+];
+
+void main() {
+  group('part 1', () {
+    test('load operation', () {
+      const str = '  Operation: new = old + 3';
+      final operation = Operation.fromString(str);
+      expect(operation.operand, equals(3));
+      expect(operation.operator, equals(Operator.plus));
+    });
+
+    test('load data', () {
+      final monkeys = Monkeys.fromData(testData);
+      final first = monkeys.first;
+      expect(first.id, isZero);
+      expect(first.worryLevels, equals([79, 98]));
+      expect(first.operation.operator, equals(Operator.multiply));
+      expect(first.operation.operand, equals(19));
+      expect(first.testModulo, equals(23));
+      expect(first.throwIfTrue, equals(2));
+      expect(first.throwIfFalse, equals(3));
+
+      final last = monkeys.last;
+      expect(last.id, equals(3));
+      expect(last.worryLevels.length, equals(1));
+      expect(last.operation.operator, equals(Operator.plus));
+    });
+
+    test('calculate', () {
+      expect(const Operation(Operator.plus, 2).calculate(6), equals(8));
+      expect(const Operation(Operator.multiply, 2).calculate(6), equals(12));
+      expect(const Operation(Operator.square, 0).calculate(6), equals(36));
+    });
+
+    test('round 1', () {
+      final monkeys = Monkeys.fromData(testData)..processRound();
+
+      expect(monkeys.toString(), startsWith('Monkey 0: [20, 23, 27, 26]'));
+    });
+
+    test('round 2', () {
+      final monkeys = Monkeys.fromData(testData)
+        ..processRound()
+        ..processRound();
+
+      expect(
+          monkeys.toString(), startsWith('Monkey 0: [695, 10, 71, 135, 350]'));
+    });
+
+    test('round 20', () {
+      final monkeys = Monkeys.fromData(testData);
+      for (var i = 0; i < 20; i++) {
+        monkeys.processRound();
+      }
+
+      expect(monkeys.toString(), startsWith('Monkey 0: [10, 12, 14, 26, 34]'));
+    });
+
+    test('items inspected', () {
+      final monkeys = Monkeys.fromData(testData);
+      for (var i = 0; i < 20; i++) {
+        monkeys.processRound();
+      }
+
+      expect(monkeys.monkeys.map((m) => m.itemsInspected),
+          equals([101, 95, 7, 105]));
+    });
+
+    test('total items after 20 rounds', () {
+      final monkeys = Monkeys.fromData(testData)..processRounds(20);
+      expect(monkeys.topTwoMonkeysProduct(), equals(10605));
+    });
+  });
+
+  group('part 2', () {
+    test('total items after 10000 rounds', () {
+      final monkeys1 = Monkeys.fromData(testData, divideByThreeRule: false)
+        ..processRounds(1);
+      expect(monkeys1.itemsInspected, equals([2, 4, 3, 6]));
+
+      final monkeys20 = Monkeys.fromData(testData, divideByThreeRule: false)
+        ..processRounds(20);
+      expect(monkeys20.itemsInspected, equals([99, 97, 8, 103]));
+
+      final monkeys1000 = Monkeys.fromData(testData, divideByThreeRule: false)
+        ..processRounds(1000);
+      expect(monkeys1000.itemsInspected, equals([5204, 4792, 199, 5192]));
+    });
+  });
+}
